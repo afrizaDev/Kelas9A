2729f7bd38ff45d694f79ebbbf0f1882388a6591
afrizaDev <101647903+afrizaDev@users.noreply.github.com>
2025-01-29 20:51:44 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+const hariIni = new Date().toLocaleDateString("id-ID", { weekday: "long" }).toLowerCase();
+    
+const hariId = {
+    "senin": "senin",
+    "selasa": "selasa",
+    "rabu": "rabu",
+    "kamis": "kamis",
+    "jumat": "jumat"
+};
 
+if (hariId[hariIni]) {
+    document.getElementById(hariId[hariIni]).classList.add("highlight");
+}
+});
